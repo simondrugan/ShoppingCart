157c8fe492325c0997d79edf78befbafd008e764
Simon Drugan <simon.drugan@fdmgroup.com>
2022-09-08 01:25:07 +0100

    added comments to describe code

--- src/main/scala/objects/Discounts.scala
+++ src/main/scala/objects/Discounts.scala
@@ -3,12 +3,12 @@ package objects
 object Discounts {
   def apply(totalNumberOfApples: Int, totalNumberOfOranges: Int): Double = {
 
+    //calculate the total discount depending on the number of items purchased
     val appleDiscount: Double = (totalNumberOfApples / 2) * Apple.price
-
     val orangeDiscount: Double = (totalNumberOfOranges / 3) * Orange.price
-
     val totalDiscount: Double = appleDiscount + orangeDiscount
 
+    // return the total discount
     totalDiscount
 
   }
--- src/main/scala/service/Checkout.scala
+++ src/main/scala/service/Checkout.scala
@@ -6,13 +6,17 @@ object Checkout {
 
   def run(itemsToCheckout: List[String]): String = {
 
+    // initialise counters
     var sumOfItemPrices: Double = 0.0
     var totalNumberOfApples: Int = 0
     var totalNumberOfOranges: Int = 0
 
+    // loop around all strings in the list provided
     for(item <- itemsToCheckout) {
+      // make the code not case sensitive
       val itemLowerCase = item.toLowerCase()
 
+      // match the items in the list to the correct object
       itemLowerCase match {
         case "orange" =>
           sumOfItemPrices += Orange.price
@@ -23,12 +27,16 @@ object Checkout {
       }
     }
 
+    // calculate the total discount using the item counters
     val totalDiscount = Discounts.apply(totalNumberOfApples, totalNumberOfOranges)
 
+    // find the total price by subtracting all discounts
     val totalPrice: Double = (sumOfItemPrices - totalDiscount) / 100
 
+    // correctly format the final price
     val finalPrice: String = (f"£$totalPrice%.2f")
 
+    // return the final price
     finalPrice
   }
 
